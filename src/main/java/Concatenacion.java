15596ae58c7b2419eaa8f60c8011caea44344023
unknown <t.jaures01@ufromail.cl>
2026-03-25 11:40:57 -0300

    Concatenacion

---
+++ src/main/java/Concatenacion.java
@@ -0,0 +1,19 @@
+public class Concatenacion {
+    public static String repetida(String[] strings){
+        String result = "";
+        for (int i = 0; i < strings.length; i++) {
+            result += strings[i];
+        }
+
+        return result;
+    }
+
+    public static String joins(String[] strings){
+        //StringBuilder
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strings.length; i++) {
+            sb.append(strings[i]);
+        }
+        return sb.toString();
+    }
+}
